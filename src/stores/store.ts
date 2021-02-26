ff7935b5e6e28602654fe64448a663536f697d42
Ali_John <2171746+gageracer@users.noreply.github.com>
2021-02-25 22:56:08 -0800

    myTok

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -33,7 +33,7 @@ firebase.auth().onAuthStateChanged((user) => {
         var uid = user.uid;
         firebase.auth().currentUser!.getIdToken(/* forceRefresh */ true).then(function (idToken) {
             myTok.set(idToken)
-            console.log("idToken is:", idToken)
+            console.log("idToken is:", myTok)
         }).catch(function (error) {
             // Handle error
             console.log("this function is bad")
